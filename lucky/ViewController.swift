3cf337cc4d576a669747e5cf4027fae20be86e12
Choironi <rdxjw4418@naver.com>
2022-03-02 23:05:19 +0900

    feature: random

--- lucky/ViewController.swift
+++ lucky/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var currentValue = 0
+    
+    @IBOutlet weak var numberLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        drawNum()
+       
     }
-
-
+    @IBAction func draw(_ sender: Any) {
+        let message = "\(currentValue)"
+        let alert = UIAlertController(title: "행운의 숫자", message: message, preferredStyle:.alert)
+        let action = UIAlertAction(title : "다음", style: .default, handler: { action in self.drawNum() //closure 사용
+            
+        })
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func drawNum() {
+        let randomNumber = arc4random_uniform(45) + 1
+        currentValue = Int(randomNumber)
+        numberLabel.text = "\(currentValue)"
+    
+    }
 }
 
